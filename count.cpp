0110f49b8658bd24d9674777fa71aa1194d0e624
saba383810 <saba383810@gmail.com>
2019-10-03 15:49:27 +0900

    文字列(a~Z)を入力しその文字列に含まれる文字が何回使われたかを調べるプログラム。

--- count.cpp
+++ count.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<unistd.h>
+#include<string>
 using namespace std;
 
 int main(){
@@ -7,10 +9,13 @@ int main(){
 	char moji[30];
 	int  cnt [26];
 	int i,j;
+	int wait = 1;
+	string ans;
+	do{
 	//初期化
 	for(j=0;j<26;j++) cnt[j] = 0;
 	//処理
-	cout << "文字を一文字ずつ入力してください。\n最後に終了の合図として「＃」を入力してください" << endl;
+	cout << "文字列を入力してください。\n最後に終了の合図として「＃」を入力してください" << endl;
 	j=0;
 	do{
 		cin >> moji[j];
@@ -22,15 +27,30 @@ int main(){
 		cout << moji[j];
 		j++;
 	}
-	cout << "」です。" << endl;
+	cout << "」ですね。" << endl;
 	i = 0;
+	cout << "この文字でお調べいたします。よろしいですか？(yes/no)" << endl;
+	cin >> ans;
+	}while(ans != "yes");
 	while(moji[i] != '#'){
 		j = 0;
 		while(moji[i] != eiji[j]) j++;//同じ文字が見つかるまでインクリメント
 		cnt[j]++;
 		i++;
 	}
+	cout << "文字をお調べします!!(>O<)!" << endl;
+	sleep(wait);
+	cout << "." << endl;
+	sleep(wait);
+	cout << "." << endl;
+	sleep(wait);
+	cout << "." << endl;
+	sleep(wait);
+	cout << "." << endl;
+	sleep(wait);
 	for(j=0;j<26;j++){
 		if(cnt[j] != 0) cout << "文字" << eiji[j] << "," << cnt[j] << endl;
 	}
+	cout << "文字は昇順に並べておきました！" << endl;
+	cout << "ご利用ありがとうございます！" << endl;
 }
